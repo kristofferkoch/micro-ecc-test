2f73d63108fc1c6d987ea1e1d24a344b017623ad
Kristoffer E. Koch <kristoffer@huddly.com>
2018-10-14 11:01:54 +0200

    verify.py: Normalize signature before verifying

--- verify.py
+++ verify.py
@@ -14,9 +14,13 @@ def _main():
     signature_raw = bytes(int(x, 16) for x in Path(sys.argv[3]).read_text().strip().split(",") if x)
     pubkey = secp256k1.PublicKey(pubkey_compressed, raw=True)
     signature = pubkey.ecdsa_deserialize_compact(signature_raw)
+    was_normalized, normalized = pubkey.ecdsa_signature_normalize(signature)
 
+    if was_normalized:
+        print("Signature was normalized")
 
-    if not pubkey.ecdsa_verify(data, signature):
+    #if not pubkey.ecdsa_verify(data, signature):
+    if not pubkey.ecdsa_verify(data, normalized):
         print("verification failed", file=sys.stderr)
         sys.exit(1)
 
